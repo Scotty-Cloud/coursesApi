baae42de0d12d07fd292c18bf1d54bb29d5bc89b
scott <scottysora@gmail.com>
2024-06-06 11:13:36 -0400

    start function to display course details

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-fetch('http://localhost:3000/courses')
-.then(response => response.json())
-.then(courses => {
-    const courseContainer = document.querySelector('#course-container')
-    courses.forEach(course => {
-        const courseCard = document.createElement('div');
-        courseCard.className = 'course-card'
+fetch("http://localhost:3000/courses")
+  .then((response) => response.json())
+  .then((courses) => {
+    const courseContainer = document.querySelector("#course-container");
+    courses.forEach((course) => {
+      const courseCard = document.createElement("div");
+      courseCard.className = "course-card";
 
-        const courseTitle = document.createElement('h2');
-        courseTitle.innerText = course.title;
-        courseCard.appendChild(courseTitle)
+      const courseTitle = document.createElement("h2");
+      courseTitle.innerText = course.title;
+      courseCard.appendChild(courseTitle);
 
-        const detailLink = document.createElement('a')
-        detailLink.href = '#'
-        detailLink.innerText = 'Detail';
-        detailLink.onclick = () => {
-            showCourseDetail(course)
-        };
-        courseCard.appendChild(detailLink)
+      const detailLink = document.createElement("a");
+      detailLink.href = "#";
+      detailLink.innerText = "Detail";
+      detailLink.onclick = () => {
+        showCourseDetail(course);
+      };
+      courseCard.appendChild(detailLink);
 
-        courseContainer.appendChild(courseCard)
+      courseContainer.appendChild(courseCard);
     });
-});
+  });
+
+function showCourseDetail(course) {
+    const modal = document.querySelector('#modal')
+    const instructorElement = document.querySelector('#instructor')
+    const startDateElement = document.querySelector('#startDate')
+    const numDaysElement = document.querySelector('#numDays')
+}
