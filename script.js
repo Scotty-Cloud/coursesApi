d2a30a9ec146894ed647ae9733cfdf953e0c482b
scott <scottysora@gmail.com>
2024-06-06 11:18:07 -0400

    add event listener to close modal

--- script.js
+++ script.js
@@ -23,12 +23,21 @@ fetch("http://localhost:3000/courses")
   });
 
 function showCourseDetail(course) {
-    const modal = document.querySelector('#modal')
-    const instructorElement = document.querySelector('#instructor')
-    const startDateElement = document.querySelector('#startDate')
-    const numDaysElement = document.querySelector('#numDays')
+  const modal = document.querySelector("#modal");
+  const instructorElement = document.querySelector("#instructor");
+  const startDateElement = document.querySelector("#startDate");
+  const numDaysElement = document.querySelector("#numDays");
 
-    instructorElement.innerText = `Instructor: ${course.instructor}`
-    startDateElement.innerText = `Start Date: ${course.startDate}`
-    numDaysElement.innerText = `Course Length: ${course.numDays} days`
+  instructorElement.innerText = `Instructor: ${course.instructor}`;
+  startDateElement.innerText = `Start Date: ${course.startDate}`;
+  numDaysElement.innerText = `Course Length: ${course.numDays} days`;
+
+  modal.style.display = "block";
 }
+
+document.addEventListener("click", (event) => {
+  if (
+    event.target === document.getElementById("modal") || event.target.className === "close") {
+    document.getElementById("modal").style.display = "none";
+  }
+});
